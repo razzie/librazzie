1c945ea899086ddbc5064f03a00fa4dbcd7e53ac
Razzie <hello@razzie.me>
2016-06-20 18:34:27 +0200

    Further network implementation

--- include/raz/network.hpp
+++ include/raz/network.hpp
@@ -35,11 +35,31 @@ namespace raz
 	class PacketBuffer
 	{
 	public:
-		PacketBuffer(SerializationMode mode) :
+		struct Head
+		{
+			PacketType packet_type;
+			uint16_t packet_size;
+		};
+
+		struct Tail
+		{
+			uint32_t n = 0;
+			bool ok() { return n == 0; }
+		};
+
+		struct PacketData
+		{
+			Head head;
+			char data[SIZE];
+			Tail tail;
+		};
+
+		PacketBuffer(SerializationMode mode = SerializationMode::DESERIALIZE, PacketType type = 0) :
 			m_mode(mode),
-			m_data_len(0),
 			m_data_pos(0)
 		{
+			m_data.packet_type = type;
+			m_data.packet_size = 0;
 		}
 
 		SerializationMode getMode() const
@@ -52,121 +72,135 @@ namespace raz
 			m_mode = mode;
 		}
 
-		char* getData()
+		PacketType getType() const
 		{
-			return m_data;
+			return m_data.type;
 		}
 
-		const char* getData() const
+		void setType(PacketType type)
 		{
-			return m_data;
+			m_data.type = type;
 		}
 
-		char* getRemainingData()
+		PacketData* getPacketData()
 		{
-			return &m_data[m_data_pos];
+			return &m_data;
 		}
 
-		const char* getRemainingData() const
+		const PacketData& getPacketData() const
 		{
-			return &m_data[m_data_pos];
+			return &m_data;
 		}
 
-		size_t getRemainingDataLength() const
-		{
-			m_data_len - m_data_pos;
-		}
-
-		size_t getDataLength() const
-		{
-			m_data_len;
-		}
-
-		void setDataLength(size_t len)
-		{
-			m_data_len = len;
-			if (m_data_pos > m_data_len)
-				m_data_pos = m_data_len;
-		}
-
-		static size_t getCapacity()
+		static size_t getDataCapacity()
 		{
 			return SIZE;
 		}
 
 		size_t write(const char* ptr, size_t len)
 		{
-			if (SIZE - m_data_len < len)
-				len = SIZE - m_data_len;
+			if (SIZE - m_data.packet_size < len)
+				len = SIZE - m_data.packet_size;
 
-			std::memcpy(&m_data[m_data_len], ptr, len);
-			m_data_len += len;
+			std::memcpy(&m_data.data[m_data.packet_size], ptr, len);
+			m_data.packet_size += len;
 			return len;
 		}
 
 		size_t read(char* ptr, size_t len)
 		{
-			if (m_data_len - m_data_pos < len)
-				len = m_data_len - m_data_pos;
+			if (m_data.packet_size - m_data_pos < len)
+				len = m_data.packet_size - m_data_pos;
 
-			std::memcpy(ptr, &m_data[m_data_pos], len);
+			std::memcpy(ptr, &m_data.data[m_data_pos], len);
 			m_data_pos += len;
 			return len;
 		}
 
 	private:
-		char m_data[SIZE];
+		PacketData m_data;
 		SerializationMode m_mode;
-		size_t m_data_len;
 		size_t m_data_pos;
 	};
 
 	template<size_t SIZE = 4096>
-	class Packet : public Serializer<PacketBuffer<SIZE>>
-	{
-	public:
-		struct Head
-		{
-			PacketType type;
-			uint16_t size;
-		};
+	using Packet = typename Serializer<PacketBuffer<SIZE>>;
 
-		struct Tail
-		{
-			uint32_t n = 0;
-			bool ok() { return n == 0; }
-		};
-
-		Packet() :
-			Serializer<PacketBuffer<SIZE>>(SerializationMode::DESERIALIZE),
-			m_type(0)
-		{
-		}
-
-		Packet(PacketType type, SerializationMode mode) :
-			Serializer<PacketBuffer<SIZE>>(mode),
-			m_type(type)
-		{
-		}
-
-		PacketType getType() const
-		{
-			return m_type;
-		}
-
-		void setType(PacketType type)
-		{
-			m_type = type;
-		}
-
-	private:
-		PacketType m_type;
-	};
-
-	template<class CLIENT, size_t SIZE>
-	struct NetworkData
+	template<class CLIENT, size_t SIZE = 4096>
+	struct ClientPacket
 	{
 		CLIENT client;
 		Packet<SIZE> packet;
+	};
+
+	class NetworkException : public std::exception
+	{
+	public:
+		NetworkException(const char* what) : m_what(what)
+		{
+		}
+
+		virtual const char* what() const
+		{
+			return m_what;
+		}
+
+	private:
+		const char* m_what;
+	};
+
+	template<class ClientBackend>
+	class NetworkClient
+	{
+	public:
+		template<class... Args>
+		NetworkClient(Args... args) : m_backend(std::forward<Args>(args)...)
+		{
+		}
+
+		template<class Packet>
+		bool getPacket(Packet& packet, uint32_t timeous_ms = 0)
+		{
+			decltype(packet)::PacketData* data = packet.getPacketData();
+			size_t netbuffer_len;
+
+			netbuffer_len = m_backend.wait(timeous_ms); // waits until data is available and returns its size
+
+			// check if at least the packet head could be read
+			if (netbuffer_len < sizeof(data->head))
+				return false;
+
+			m_backend.peek(reinterpret_cast<char*>(&data->head), sizeof(data->head));
+
+			// check if the whole packet data is available
+			if ((netbuffer_len - sizeof(data->head) - sizeof(data->tail) < data->head.packet_size))
+				return false;
+
+			if (data->head.packet_size > packet.getDataCapacity())
+				throw NetworkException("Too large packet");
+
+			// reading actual data to the packet
+			m_backend.read(reinterpret_cast<char*>(&data->head), sizeof(data->head));
+			m_backend.read(data->data, data->head.packet_size);
+			m_backend.read(reinterpret_cast<char*>(&data->tail), sizeof(data->tail));
+
+			if (!data->fail.ok())
+				throw NetworkException("Corrupted packet");
+
+			return true;
+		}
+
+		template<class Packet>
+		void sendPacket(const Packet& packet)
+		{
+			const decltype(packet)::PacketData* data = packet.getPacketData();
+
+			m_backend.write(reinterpret_cast<const char*>(&data->head), sizeof(data->head));
+			m_backend.write(data->data, data->head.packet_size);
+			m_backend.write(reinterpret_cast<const char*>(&data->tail), sizeof(data->tail));
+		}
+
+	private:
+		ClientBackend m_backend;
 	};
 }
