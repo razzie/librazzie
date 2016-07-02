bd0f7b2dbde1417301bd895b49aeaacf2ecf1181
Gábor Görzsöny <gabor@gorzsony.com>
2016-07-02 18:31:48 +0200

    Added EventSystem class

--- examples/event/event.cpp
+++ examples/event/event.cpp
@@ -7,13 +7,16 @@ using namespace raz::literal; // for the _event literal operator
 typedef raz::Event<"foo"_event, std::string, int> FooEvent;
 typedef raz::Event<"bar"_event, float> BarEvent;
 
-FooEvent::CallbackSystem foo_callbacks;
-BarEvent::CallbackSystem bar_callbacks;
-raz::EventDispatcher<FooEvent, BarEvent> event_dispatcher(foo_callbacks, bar_callbacks);
+//FooEvent::CallbackSystem foo_callbacks;
+//BarEvent::CallbackSystem bar_callbacks;
+//raz::EventDispatcher<FooEvent, BarEvent> event_dispatcher(foo_callbacks, bar_callbacks);
+raz::EventSystem<FooEvent, BarEvent> event_system;
 
 struct FooEventReceiver : public FooEvent::Callback
 {
-	FooEventReceiver() : FooEvent::Callback(foo_callbacks, &FooEventReceiver::receive)
+	FooEventReceiver() :
+		//FooEvent::Callback(foo_callbacks, &FooEventReceiver::receive)
+		FooEvent::Callback(event_system.getSystemByCallback(this), &FooEventReceiver::receive)
 	{
 	}
 
@@ -27,7 +30,8 @@ int main()
 {
 	FooEventReceiver r;
 	FooEvent e("razzie", 99);
-	event_dispatcher.handle(e);
+	//event_dispatcher.handle(e);
+	event_system.handle(e);
 
 	return 0;
 }
