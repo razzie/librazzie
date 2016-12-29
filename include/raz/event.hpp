b7ef76a1fda9011cecb23e974d4c0bf73cdbc78c
Gábor Görzsöny <gabor@gorzsony.com>
2016-12-29 02:01:56 +0100

    Added EventQueue and EventQueueSystem + improved event framework

--- include/raz/event.hpp
+++ include/raz/event.hpp
@@ -22,6 +22,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 #pragma once
 
 #include <cstdint>
+#include <deque>
+#include <mutex>
 #include <tuple>
 #include <utility>
 #include "raz/callback.hpp"
@@ -46,7 +48,7 @@ namespace raz
 		class CallbackSystem : public raz::CallbackSystem<Event>
 		{
 		public:
-			static EventType getEventType()
+			static constexpr EventType getEventType()
 			{
 				return Event::getType();
 			}
@@ -72,7 +74,7 @@ namespace raz
 		{
 		}
 
-		static EventType getType()
+		static constexpr EventType getType()
 		{
 			return Type;
 		}
@@ -141,6 +143,173 @@ namespace raz
 		}
 	}
 
+
+	template<class Event>
+	class EventQueue
+	{
+	public:
+		EventQueue() = default;
+
+		EventQueue(EventQueue&& other) : m_queue(std::move(other.m_queue))
+		{
+		}
+
+		static constexpr EventType getEventType()
+		{
+			return Event::getType();
+		}
+
+		template<class... Params>
+		auto enqueue(Params... params) -> decltype(void(new Event(std::forward<Params>(params)...)))
+		{
+			std::lock_guard<decltype(m_lock)> guard(m_lock);
+			m_queue.emplace_back(std::forward<Params>(params)...);
+		}
+
+		template<class Handler>
+		void dequeue(Handler& handler)
+		{
+			std::lock_guard<decltype(m_lock)> guard(m_lock);
+
+			while (!m_queue.empty())
+			{
+				handler.handle(m_queue.front());
+				m_queue.pop_front();
+			}
+		}
+
+		template<class Handler>
+		void dequeue(Handler& handler, unsigned n)
+		{
+			std::lock_guard<decltype(m_lock)> guard(m_lock);
+			unsigned count = 0;
+
+			while (!m_queue.empty() && count < n)
+			{
+				handler.handle(m_queue.front());
+				m_queue.pop_front();
+				++count;
+			}
+		}
+
+		void clear()
+		{
+			m_queue.clear();
+		}
+
+	private:
+		std::recursive_mutex m_lock;
+		std::deque<Event> m_queue;
+	};
+
+	template<class... Events>
+	class EventQueueSystem
+	{
+	public:
+		EventQueueSystem() = default;
+		EventQueueSystem(const EventQueueSystem&) = delete;
+
+		template<EventType Type, class... Params>
+		void enqueue(Params... params)
+		{
+			_enqueue<Type, 0>(std::forward<Params>(params)...);
+		}
+
+		template<class Serializer>
+		void enqueueSerialized(EventType type, Serializer& serializer)
+		{
+			_enqueueSerialized<0>(type, serializer);
+		}
+
+		template<class Handler>
+		void dequeue(Handler& handler) // Event::CallbackSystem, EventDispatcher, etc
+		{
+			_dequeue<0>(handler);
+		}
+
+		void clear()
+		{
+			_clear<0>();
+		}
+
+	private:
+		std::tuple<EventQueue<Events>...> m_queues;
+
+		template<EventType Type>
+		void _enqueue(...)
+		{
+		}
+
+		template<EventType Type, class Queue, class... Params>
+		auto _enqueue(Queue& queue, Params... params) -> decltype(queue.enqueue(std::forward<Params>(params)...))
+		{
+			if (Queue::getEventType() == Type)
+				queue.enqueue(std::forward<Params>(params)...);
+		}
+
+		template<EventType Type, size_t N, class... Params>
+		std::enable_if_t<(N < sizeof...(Events))> _enqueue(Params... params)
+		{
+			_enqueue<Type>(std::get<N>(m_queues), std::forward<Params>(params)...);
+			_enqueue<Type, N + 1>(std::forward<Params>(params)...);
+		}
+
+		template<EventType Type, size_t N, class... Params>
+		std::enable_if_t<(N >= sizeof...(Events))> _enqueue(Params...)
+		{
+		}
+
+		template<size_t N, class Serializer>
+		std::enable_if_t<(N < sizeof...(Events))> _enqueueSerialized(EventType type, Serializer& serializer)
+		{
+			auto& queue = std::get<N>(m_queues);
+			if (queue.getEventType() == type)
+				queue.enqueueSerialized(type, serializer);
+
+			_enqueueSerialized<N + 1>(type, serializer);
+		}
+
+		template<size_t N, class Serializer>
+		std::enable_if_t<(N >= sizeof...(Events))> _enqueueSerialized(EventType, Serializer&)
+		{
+		}
+
+		void _dequeue(...)
+		{
+		}
+
+		template<class Queue, class Handler>
+		auto _dequeue(Queue& queue, Handler& handler) -> decltype(queue.dequeue(handler))
+		{
+			queue.dequeue(handler);
+		}
+
+		template<size_t N, class Handler>
+		std::enable_if_t<(N < sizeof...(Events))> _dequeue(Handler& handler)
+		{
+			_dequeue(std::get<N>(m_queues), handler);
+			_dequeue<N + 1>(handler);
+		}
+
+		template<size_t N, class Handler>
+		std::enable_if_t<(N >= sizeof...(Events))> _dequeue(Handler&)
+		{
+		}
+
+		template<size_t N>
+		std::enable_if_t<(N < sizeof...(Events))> _clear()
+		{
+			std::get<N>(m_queues).clear();
+			_clear<N + 1>();
+		}
+
+		template<size_t N>
+		std::enable_if_t<(N >= sizeof...(Events))> _clear()
+		{
+		}
+	};
+
+
 	template<class... Events>
 	class EventDispatcher // the given event callback systems must always live longer
 	{
@@ -150,16 +319,18 @@ namespace raz
 		{
 		}
 
+		EventDispatcher(const EventDispatcher&) = delete;
+
 		template<class Event>
 		void handle(const Event& e)
 		{
-			handleInternal<Event, 0, Events...>(e);
+			_handle<Event, 0, Events...>(e);
 		}
 
 		template<class Serializer>
 		void handleSerialized(EventType type, Serializer& s)
 		{
-			handleSerializedInternal<Serializer, 0, Events...>(type, s);
+			_handleSerialized<Serializer, 0, Events...>(type, s);
 		}
 
 	protected:
@@ -172,40 +343,40 @@ namespace raz
 		std::tuple<typename Events::CallbackSystem*...> m_callback_systems;
 
 		template<class Event, size_t N>
-		void handleInternal(const Event&)
+		void _handle(const Event&)
 		{
 		}
 
 		template<class Event, size_t N, class RegEvent0, class... RegEvents>
 		std::enable_if_t<std::is_same<Event, RegEvent0>::value>
-			handleInternal(const Event& e)
+			_handle(const Event& e)
 		{
 			auto cb = std::get<N>(m_callback_systems);
 			cb->handle(e);
 
-			handleInternal<Event, N + 1, RegEvents...>(e);
+			_handle<Event, N + 1, RegEvents...>(e);
 		}
 
 		template<class Event, size_t N, class RegEvent0, class... RegEvents>
 		std::enable_if_t<!std::is_same<Event, RegEvent0>::value>
-			handleInternal(const Event& e)
+			_handle(const Event& e)
 		{
-			handleInternal<Event, N + 1, RegEvents...>(e);
+			_handle<Event, N + 1, RegEvents...>(e);
 		}
 
 		template<class Serializer, size_t N>
-		void handleSerializedInternal(EventType, Serializer&)
+		void _handleSerialized(EventType, Serializer&)
 		{
 		}
 
 		template<class Serializer, size_t N, class RegEvent0, class... RegEvents>
-		void handleSerializedInternal(EventType type, Serializer& s)
+		void _handleSerialized(EventType type, Serializer& s)
 		{
 			auto cb = std::get<N>(m_callback_systems);
 			if (cb->getEventType() == type)
 				cb->handleSerialized<Serializer>(s);
 
-			handleSerializedInternal<Serializer, N + 1, RegEvents...>(type, s);
+			_handleSerialized<Serializer, N + 1, RegEvents...>(type, s);
 		}
 	};
 
@@ -218,14 +389,16 @@ namespace raz
 		{
 		}
 
+		EventSystem(const EventSystem&) = delete;
+
 		template<class Event>
-		typename Event::CallbackSystem& getSystemByEvent(Event* = nullptr)
+		typename Event::CallbackSystem& access(Event* = nullptr)
 		{
 			return std::get<typename Event::CallbackSystem>(m_callback_systems);
 		}
 
 		template<class Callback>
-		typename Callback::ValueType::CallbackSystem& getSystemByCallback(Callback* = nullptr)
+		typename Callback::ValueType::CallbackSystem& access(Callback* = nullptr)
 		{
 			return std::get<typename Callback::ValueType::CallbackSystem>(m_callback_systems);
 		}
