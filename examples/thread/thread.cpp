d7892cc14fabe543515901c2029d5de29f1c6955
Gábor Görzsöny <gabor@gorzsony.com>
2018-03-31 23:36:33 +0200

    Changed in TaskManager

--- examples/thread/thread.cpp
+++ examples/thread/thread.cpp
@@ -102,6 +102,8 @@ void example04()
 {
 	raz::TaskManager taskmgr;
 	taskmgr([] { std::cout << "hello from task" << std::endl; });
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
 }
 
 int main()
